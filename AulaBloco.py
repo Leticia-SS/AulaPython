b24e82ea56c2a2ece3e1a12d96772745e377ee46
Leticia-SS <leticia.saraiva013@gmail.com>
2024-02-20 08:01:02 -0300

    Inicio

--- AulaBloco.py
+++ AulaBloco.py
@@ -1,4 +1,23 @@
-for contador in range(200):
-    print(contador)
-    while contador<5:
-        print('Menor que 5')
+import random
+import time
+
+def paths():
+    print('\n\n\n\n\n\n\n')
+    print('Hello Fellow')
+    time.sleep(1)
+    print('\n . . .\n')
+    time.sleep(1)
+    print('Welcome to your Neeew Journey')
+    time.sleep(1)
+    print('\n . . .\n')
+    time.sleep(1)
+    hero = input("What is the Hero's name this time =>")
+    print('\nWONDERFUL')
+    time.sleep(1)
+    print('\n . . .\n')
+    time.sleep(1)
+    print('\n . . .\n')
+    time.sleep(1)
+    print('\n . . .\n')
+
+paths()
