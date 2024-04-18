603ac0f364c5c4937fa750b6049dc8e5386ba179
Leticia-SS <leticia.saraiva013@gmail.com>
2024-04-18 08:07:25 -0300

    Set upd

--- Set&Dicionario.py
+++ Set&Dicionario.py
@@ -46,5 +46,32 @@ conjunto.add(['f'],['f']) # diversas listas imutáveis
 # error: unhasgable type: list
 
 # discard(): remove um item específico
-# remove(): remove um item específico
+# remove(): remove um item específico ou gera keyerror se ele estiver ausente no conjunto
 # pop(): remove item aleatório item e salva na variável
+# clear(): limpa o conjunto 
+
+# len(): retorna o tamanho do conjunto
+# sum(): retorna soma
+# min() max(): (não envolve ordenação) maior e menor item (conjuntos numéricos)
+
+# para comparação com string e tupla, para string é o ASCII e a tupla é pelo index
+
+# all(): retorna True se todos os itens forem avaliados como verdadeiros ou o conjunto estiver vazio
+# any(): retorna se algum for True
+# sorted(): caso queira ordenar, se for string é pela ASCII (conjunto não é ordenado)
+
+# union(): une 2 ou mais conjuntos e retorna um novo conjunto exclusivo
+# intersection(): retorna um conjunto de itens comuns
+# difference(): retorna todos os itens do primeiro que estão no primeiro conjunto mas não no segundo
+a = {1,2,3,4,5,6}
+b = {4,5,6,7}
+
+a.difference(b)
+
+# symmetric_difference(): retorna um novo conjunto de itens presentes no primeiro e no segundo conjunto mas nçao em ambos (está no a e está no b mas não em ambos)
+
+# intersection_update() ou &= : reescreve o conjunto com a interseção deste conjunto com outro
+# difference_update() ou -= : reescreve o conjunto atual com a diferença deste conjunto com outro
+# symmetric_difference_update ou ^= : reescreve o conjunto atual com a diferença simetrica do atual com outro
+# issubset() ou <= : retorna True se outro conjunto contém cada item do conjunto atual, se não quiser verificar se é igual, utilize somente o <
+# issuperset() ou >= : retorna True se o conjunto atual contem cada item de outro conjunto incluindo se identicos. Caso nção deseje ver se são identicos utilize >
