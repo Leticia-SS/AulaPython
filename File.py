8c5809eba4d9bc9cdcc3513a0da5d359ef034983
Leticia-SS <leticia.saraiva013@gmail.com>
2024-04-24 07:47:37 -0300

    File

--- File.py
+++ File.py
@@ -32,3 +32,40 @@ arq.close()
 
 # Palavras totais, para palavras unicas usar set() e use len()
 print(f'Quantidade de palavras: {num_palavras}')
+
+
+# Escrita em Arquivo
+
+arq = open('arq_texto.txt', 'w')
+
+# o comando write não introduz quebra de linha no fim do texto
+arq.write('Linha 1')
+arq.write('linha 2')
+# Output = Linha1Linha2
+
+arq.write('Linha 1\n')
+arq.write('Linha 2\n')
+arq.write('linha 3\n')
+# Output = 
+# Linha 1
+# Linha 2
+# Linha 3 
+
+arq.close()
+
+# Pegar informações da CPU e Memória do PC
+import time, psutil
+
+arq = open('log.txt', 'a')
+
+for i in range(10):
+    tempo_atual = time.ctime(time.time())
+    uso_mem = '{:6.2f}'.format(psutil.virtual_memory().percent)
+    uso_cpu = '{:6.2f}'.format(psutil.cpu_percent)
+    texto = tempo_atual
+    texto = texto + uso_mem + ' '
+    texto = texto + uso_cpu + '\n'
+    arq.write(texto)
+    time.sleep(1)
+
+arq.close()
