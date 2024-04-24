a6202a4ae42941a165d9ff915649308428f02e86
Leticia-SS <leticia.saraiva013@gmail.com>
2024-04-24 08:58:43 -0300

    libPandas

---
+++ Pandas.py
@@ -0,0 +1,10 @@
+# Pandas é a biblioteca voltada para manipulação de dados em Python
+
+# Implementa duas novas estruturas de dados, a serie e o dataframe
+# Funções para leitura e escrita de formatos amplamente usados como CSV, texto, Excel, bases de SQL, etc
+#Flexibiliza reformatação de conjuntos de dados
+# Permite inclusão e remoç]ao de colunas e linhas em suas estruturas de dados
+#Agrega e transforma dados a ártis de implementação de 'group by', permitindo aplicação do SAC ('Split-Apply-Combine')
+ 
+
+# Uma serie no python sempre vai ser uma serie booleana
